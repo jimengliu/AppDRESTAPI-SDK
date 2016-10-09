4a91a9ba3bc5b51f2428434b90611d5e5a9411d2
soloink <gsolorzano@solosoftwareinc.com>
2016-10-08 17:35:50 -0700

    Adding new dashboards

---
+++ src/org/appdynamics/appdrestapi/data/CustomDashboard_4_2_Newer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.appdynamics.appdrestapi.data;
+
+/**
+ *
+ * @author soloink
+ */
+public class CustomDashboard_4_2_Newer {
+    
+}
+
+/*
+    "schemaVersion": null,
+    "dashboardFormatVersion": "3.0",
+    "name": "DTVUS-IT-Prod_Engineering-POPs",
+    "description": null,
+    "properties": null,
+    "templateEntityType": "APPLICATION_COMPONENT_NODE",
+    "associatedEntityTemplates": null,
+    "minutesBeforeAnchorTime": 15,
+    "startDate": null,
+    "endDate": null,
+    "refreshInterval": 120000,
+    "backgroundColor": 15395562,
+    "color": 15395562,
+    "height": 768,
+    "width": 1024,
+    "canvasType": "CANVAS_TYPE_GRID",
+    "layoutType": "",
+    widgetTemplates : array of widgets
+    "template": false,
+    "warRoom": false
+*/
---
+++ src/org/appdynamics/appdrestapi/data/DataSeries_4_2_Newer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.appdynamics.appdrestapi.data;
+
+/**
+ *
+ * @author soloink
+ */
+public class DataSeries_4_2_Newer {
+    
+}
+
+/*
+
+                    "seriesType": "COLUMN",
+                    "metricType": "JMX",
+                    "showRawMetricName": false,
+                    "colorPalette": null,
+                    "name": "LAP-CurrentThreadCounts",
+                    "metricMatchCriteriaTemplate"
+                    "axisPosition": "LEFT"
+*/
