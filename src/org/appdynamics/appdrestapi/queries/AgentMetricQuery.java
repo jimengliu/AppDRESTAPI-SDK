bdba25f34918bcfb9227feeb71bfc5ed78f83526
soloink <gsolorzano@solosoftwareinc.com>
2016-10-16 06:36:22 -0700

    Updated the queries

--- src/org/appdynamics/appdrestapi/queries/AgentMetricQuery.java
+++ src/org/appdynamics/appdrestapi/queries/AgentMetricQuery.java
@@ -389,11 +389,242 @@ public class AgentMetricQuery{
         return val.toString();
         
     }
+    
+    /**
+     * 
+     * <p>
+     * This query will return the Registration Successful from a Tier perspective for example:
+     * Application Infrastructure Performance|[TIER]|Agent|Discovered Backends|Registration Successful
+     * </p>
+     * @param baseURL Base URL for the controller
+     * @param application Name of the application
+     * @param tier Name of the tier
+     * @param node Name of the node
+     * @param start Start time for the metric query
+     * @param end End time for the metric query
+     * @param rollup Whether to rollup the values
+     * @return String 
+     * 
+     */
+    public static String queryTierAgentDiscoveredBackendsRegistrationSuccessful(String baseURL, String application,String tier,
+            long start, long end, boolean rollup){
+        
+        StringBuilder val = new StringBuilder();
+        val.append(baseURL).append(s.CONTROLLER_APPS);
+        val.append(QueryEncoder.encode(application));
+        val.append(s.URL_METRIC_PATH);
+        
+        // This has to be encoded otherwise the query will fail.
+        StringBuilder bud = new StringBuilder();
+        bud.append(s.APPLICATION_INFRA_PERF).append(tier);
+        bud.append(s.AGENT).append(s.AGENT_DISCOVERED_BACKENDS).append(s.AGENT_DISCOVERED_BACKENDS_REGISTRATION_SUCCESSFUL);
+        val.append(QueryEncoder.encode(bud.toString()));  
+        
+        //val.append(s.LAST_15_MINUTES);
+        val.append(s.TIME_BETWEEN).append(s.TIME_START_TIME).append(start);
+        val.append(s.TIME_END_TIME).append(end);
+        if(!rollup) val.append(s.NO_ROLL_UP);
+        
+        return val.toString();
+        
+    }
+    
+    /**
+     * 
+     * <p>
+     * This query will return the Registration Successful from a Node perspective for example:
+     * Application Infrastructure Performance|[TIER]|Individual Nodes|[NODE]|Agent|Discovered Backends|Registration Successful
+     * </p>
+     * @param baseURL Base URL for the controller
+     * @param application Name of the application
+     * @param tier Name of the tier
+     * @param node Name of the node
+     * @param start Start time for the metric query
+     * @param end End time for the metric query
+     * @param rollup Whether to rollup the values
+     * @return String 
+     * 
+     */
+    public static String queryNodeAgentDiscoveredBackendsRegistrationSuccessful(String baseURL, String application,String tier, String node ,
+            long start, long end, boolean rollup){
+        
+        
+        StringBuilder val = new StringBuilder();
+        val.append(baseURL).append(s.CONTROLLER_APPS);
+        val.append(QueryEncoder.encode(application));
+        val.append(s.URL_METRIC_PATH);
+        
+        // This has to be encoded otherwise the query will fail.
+        StringBuilder bud = new StringBuilder();
+        bud.append(s.APPLICATION_INFRA_PERF).append(tier).append(s.INDIVIDUAL_NODES).append(node);
+        bud.append(s.AGENT).append(s.AGENT_DISCOVERED_BACKENDS).append(s.AGENT_DISCOVERED_BACKENDS_REGISTRATION_SUCCESSFUL);
+        val.append(QueryEncoder.encode(bud.toString()));  
+        
+        //val.append(s.LAST_15_MINUTES);
+        val.append(s.TIME_BETWEEN).append(s.TIME_START_TIME).append(start);
+        val.append(s.TIME_END_TIME).append(end);
+        if(!rollup) val.append(s.NO_ROLL_UP);
+        
+        return val.toString();
+    }
+    
+    /**
+     * 
+     * <p>
+     * This query will return the Registration Failed from a Node perspective for example:
+     * Application Infrastructure Performance|[TIER]|Agent|Discovered Backends|Registration Failed
+     * </p>
+     * @param baseURL Base URL for the controller
+     * @param application Name of the application
+     * @param tier Name of the tier
+     * @param node Name of the node
+     * @param start Start time for the metric query
+     * @param end End time for the metric query
+     * @param rollup Whether to rollup the values
+     * @return String 
+     * 
+     */
+    public static String queryTierAgentDiscoveredBackendsRegistrationFailed(String baseURL, String application,String tier, String node ,
+            long start, long end, boolean rollup){
+        
+        StringBuilder val = new StringBuilder();
+        val.append(baseURL).append(s.CONTROLLER_APPS);
+        val.append(QueryEncoder.encode(application));
+        val.append(s.URL_METRIC_PATH);
+        
+        // This has to be encoded otherwise the query will fail.
+        StringBuilder bud = new StringBuilder();
+        bud.append(s.APPLICATION_INFRA_PERF).append(node);
+        bud.append(s.AGENT).append(s.AGENT_DISCOVERED_BACKENDS).append(s.AGENT_DISCOVERED_BACKENDS_REGISTRATION_FAILED);
+        val.append(QueryEncoder.encode(bud.toString()));  
+        
+        //val.append(s.LAST_15_MINUTES);
+        val.append(s.TIME_BETWEEN).append(s.TIME_START_TIME).append(start);
+        val.append(s.TIME_END_TIME).append(end);
+        if(!rollup) val.append(s.NO_ROLL_UP);
+        
+        return val.toString();
+        
+    }
+    
+    /**
+     * 
+     * <p>
+     * This query will return the Registration Failed from a Node perspective for example:
+     * Application Infrastructure Performance|[TIER]|Individual Nodes|[NODE]|Agent|Discovered Backends|Registration Failed
+     * </p>
+     * @param baseURL Base URL for the controller
+     * @param application Name of the application
+     * @param tier Name of the tier
+     * @param node Name of the node
+     * @param start Start time for the metric query
+     * @param end End time for the metric query
+     * @param rollup Whether to rollup the values
+     * @return String 
+     * 
+     */
+    public static String queryNodeAgentDiscoveredBackendsRegistrationFailed(String baseURL, String application,String tier, String node ,
+            long start, long end, boolean rollup){
+        
+        StringBuilder val = new StringBuilder();
+        val.append(baseURL).append(s.CONTROLLER_APPS);
+        val.append(QueryEncoder.encode(application));
+        val.append(s.URL_METRIC_PATH);
+        
+        // This has to be encoded otherwise the query will fail.
+        StringBuilder bud = new StringBuilder();
+        bud.append(s.APPLICATION_INFRA_PERF).append(tier).append(s.INDIVIDUAL_NODES).append(node);
+        bud.append(s.AGENT).append(s.AGENT_DISCOVERED_BACKENDS).append(s.AGENT_DISCOVERED_BACKENDS_REGISTRATION_FAILED);
+        val.append(QueryEncoder.encode(bud.toString()));  
+        
+        //val.append(s.LAST_15_MINUTES);
+        val.append(s.TIME_BETWEEN).append(s.TIME_START_TIME).append(start);
+        val.append(s.TIME_END_TIME).append(end);
+        if(!rollup) val.append(s.NO_ROLL_UP);
+        
+        return val.toString();
+        
+    }
+    
+    /**
+     * 
+     * <p>
+     * This query will return the RUnmonitored Calls per Minute from a Node perspective for example:
+     * Application Infrastructure Performance|[TIER]|Agent|Discovered Backends|Unmonitored Calls per Minute
+     * </p>
+     * @param baseURL Base URL for the controller
+     * @param application Name of the application
+     * @param tier Name of the tier
+     * @param node Name of the node
+     * @param start Start time for the metric query
+     * @param end End time for the metric query
+     * @param rollup Whether to rollup the values
+     * @return String 
+     * 
+     */
+    public static String queryTierAgentDiscoveredBackendsUnmonitoredCallsPerMinute(String baseURL, String application,String tier, String node ,
+            long start, long end, boolean rollup){
+        
+        StringBuilder val = new StringBuilder();
+        val.append(baseURL).append(s.CONTROLLER_APPS);
+        val.append(QueryEncoder.encode(application));
+        val.append(s.URL_METRIC_PATH);
+        
+        // This has to be encoded otherwise the query will fail.
+        StringBuilder bud = new StringBuilder();
+        bud.append(s.APPLICATION_INFRA_PERF).append(tier).append(s.INDIVIDUAL_NODES).append(node);
+        bud.append(s.AGENT).append(s.AGENT_DISCOVERED_BACKENDS).append(s.AGENT_DISCOVERED_BACKENDS_UNMONITORED_CALLS_PER_MINUTE);
+        val.append(QueryEncoder.encode(bud.toString()));  
+        
+        //val.append(s.LAST_15_MINUTES);
+        val.append(s.TIME_BETWEEN).append(s.TIME_START_TIME).append(start);
+        val.append(s.TIME_END_TIME).append(end);
+        if(!rollup) val.append(s.NO_ROLL_UP);
+        
+        return val.toString();
+        
+    }
+    
+    /**
+     * 
+     * <p>
+     * This query will return the RUnmonitored Calls per Minute from a Node perspective for example:
+     * Application Infrastructure Performance|[TIER]|Individual Nodes|[NODE]|Agent|Discovered Backends|Unmonitored Calls per Minute
+     * </p>
+     * @param baseURL Base URL for the controller
+     * @param application Name of the application
+     * @param tier Name of the tier
+     * @param node Name of the node
+     * @param start Start time for the metric query
+     * @param end End time for the metric query
+     * @param rollup Whether to rollup the values
+     * @return String 
+     * 
+     */
+    public static String queryNodeAgentDiscoveredBackendsUnmonitoredCallsPerMinute(String baseURL, String application,String tier, String node ,
+            long start, long end, boolean rollup){
+        
+        StringBuilder val = new StringBuilder();
+        val.append(baseURL).append(s.CONTROLLER_APPS);
+        val.append(QueryEncoder.encode(application));
+        val.append(s.URL_METRIC_PATH);
+        
+        // This has to be encoded otherwise the query will fail.
+        StringBuilder bud = new StringBuilder();
+        bud.append(s.APPLICATION_INFRA_PERF).append(tier).append(s.INDIVIDUAL_NODES).append(node);
+        bud.append(s.AGENT).append(s.AGENT_DISCOVERED_BACKENDS).append(s.AGENT_DISCOVERED_BACKENDS_UNMONITORED_CALLS_PER_MINUTE);
+        val.append(QueryEncoder.encode(bud.toString()));  
+        
+        //val.append(s.LAST_15_MINUTES);
+        val.append(s.TIME_BETWEEN).append(s.TIME_START_TIME).append(start);
+        val.append(s.TIME_END_TIME).append(end);
+        if(!rollup) val.append(s.NO_ROLL_UP);
+        
+        return val.toString();
+        
+    }
+    
 /*
-
-Application Infrastructure Performance|[TIER]|Agent|Discovered Backends|Registration Successful
-Application Infrastructure Performance|[TIER]|Agent|Discovered Backends|Registration Failed
-Application Infrastructure Performance|[TIER]|Agent|Discovered Backends|Unmonitored Calls per Minute
 Application Infrastructure Performance|[TIER]|Agent|ConfigChannel|Number Of Application Infrastructure Changes Sent
 Application Infrastructure Performance|[TIER]|Agent|Snapshot Upload|Snapshots uploaded
 Application Infrastructure Performance|[TIER]|Agent|Snapshot Upload|Invalid Snapshots
